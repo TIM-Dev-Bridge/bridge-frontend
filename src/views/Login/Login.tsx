06f3770fadb1402eb3e11b6fd4caecdaa940a3f2
wittawin381 <31582969+wittawin381@users.noreply.github.com>
2021-10-25 15:17:30 +0700

    add simple login/register page

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -3,9 +3,20 @@ import LoginForm from './components/LoginForm';
 import RegisterForm from './components/RegisterForm';
 
 const LoginPage: React.FC = ()=> {
+    enum DisplayComponent {
+        Login = 1,
+        Register,
+    }
+
+    const [displayComponent, setDisplayComponent] = React.useState<DisplayComponent>(DisplayComponent.Login)
+
+    React.useEffect(()=> {
+        console.log(displayComponent === DisplayComponent.Login)
+    })
+    
     return (
         <div className="flex flex-row w-screen h-screen">
-            <div className="w-1/2 h-full flex justify-center items-center "> 
+            <div className="w-1/2 h-full flex justify-center items-center"> 
                 <RegisterForm />
             </div>
             <div className="w-1/2 h-full flex justify-center items-center">
--- src/views/Login/components/RegisterForm.tsx
+++ src/views/Login/components/RegisterForm.tsx
@@ -4,17 +4,18 @@ const RegisterForm: React.FC =()=> {
     return (
         <div className="flex flex-col pb-10 w-3/4 rounded-3xl shadow-md">
             <div className="flex flex-col mr-10 ml-10 -3xl">
-                <div className="m-5 text-center">Login</div>
-                <TextField />
-                <TextField />
-                <div className="flex flex-row justify-end items-center">
-                    <input type="checkbox" />
-                    <p>Remember me</p>
+                <div className="m-5 text-center">Register</div>
+                <div className="flex flex-row">
+                    <TextField />
+                    <TextField />
                 </div>
+                <TextField />
+                <TextField />
+                <TextField />
+                <TextField />
                 <button className="border-none h-8 rounded-3xl pl-8 pr-8 mt-3 mb-3 text-white bg-blue-300">Continue</button>
-                <div className="flex flex-row items-center justify-between">
-                    <p>Don't have account</p>
-                    <p>Forgot password</p>
+                <div className="flex flex-row items-center justify-end">
+                    <p>Already Have an account</p>
                 </div>
             </div>
         </div>
@@ -27,7 +28,7 @@ interface TextFieldProps extends InputHTMLAttributes<HTMLInputElement> {
 
 const TextField: React.FC<TextFieldProps> =(props: TextFieldProps)=> {
     return (
-        <input type="text" className="border-none h-8 rounded-3xl pl-8 pr-8 bg-gray-100 mt-3 mb-3" value={props.text} placeholder="placehlder"/>
+        <input type="text" className="border-none h-8 rounded-3xl pl-4 pr-4 w-full bg-gray-100 mt-3 mb-3" value={props.text} placeholder="placehlder"/>
     );
 }
 
