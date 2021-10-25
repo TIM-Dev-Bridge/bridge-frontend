ceae6afea00d12ec445365e7126dc6fd1ebd6938
wittawin381 <31582969+wittawin381@users.noreply.github.com>
2021-10-25 14:57:36 +0700

    init register component

--- src/views/Login/Login.tsx
+++ src/views/Login/Login.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import LoginForm from './components/LoginForm';
+import RegisterForm from './components/RegisterForm';
 
 const LoginPage: React.FC = ()=> {
     return (
-        <div className="flex-row w-screen h-screen">
-            <div className="w-1/2 "> 
-
+        <div className="flex flex-row w-screen h-screen">
+            <div className="w-1/2 h-full flex justify-center items-center "> 
+                <RegisterForm />
             </div>
             <div className="w-1/2 h-full flex justify-center items-center">
                 <LoginForm />
